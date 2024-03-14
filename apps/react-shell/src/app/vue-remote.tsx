ada6694f7dbf81ca70df34967c5b3488ea1661ec
pot-code <136290354@qq.com>
2024-03-14 10:43:03 +0800

    refactor: load remote vue component

--- apps/react-shell/src/app/vue-remote.tsx
+++ apps/react-shell/src/app/vue-remote.tsx
@@ -1,22 +1,19 @@
 import { useEffect, useRef } from 'react';
-import { createApp } from 'vue';
+import { createApp, defineAsyncComponent } from 'vue';
+
+const component = defineAsyncComponent(() => import('vue-remote/App'));
 
 export default function VueRemote() {
   const containerRef = useRef(null);
 
   useEffect(() => {
-    let ignored = false;
-    import('vue-remote/App').then(({ default: App }) => {
-      if (ignored) return;
-
-      if (containerRef.current) {
-        const app = createApp(App);
-        app.mount(containerRef.current);
-      }
-    });
+    const app = createApp(component);
+    if (containerRef.current) {
+      app.mount(containerRef.current);
+    }
 
     return () => {
-      ignored = true;
+      app.unmount();
     };
   });
 
